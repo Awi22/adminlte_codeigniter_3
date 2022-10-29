6ca7502445a02dc03c94d1b3940ee221bddea4b4
Awi22 <102220981+Awi22@users.noreply.github.com>
2022-10-29 16:07:19 +0800

    Update login

--- application/views/login.php
+++ application/views/login.php
@@ -31,7 +31,7 @@
             <div class="card-body login-card-body">
                 <p class="login-box-msg">Sign in to start your session</p>
 
-                <form action="<?= base_url(); ?>index.php/dashboard1" method="post">
+                <form action="<?= base_url(); ?>dashboard1" method="post">
                     <div class="input-group mb-3">
                         <input type="email" class="form-control" placeholder="Email">
                         <div class="input-group-append">
