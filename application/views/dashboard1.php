695031d3c961f8647c13d3bae954bd6f62629264
Awi22 <102220981+Awi22@users.noreply.github.com>
2022-10-29 16:09:24 +0800

    Update dashboard1

--- application/views/dashboard1.php
+++ application/views/dashboard1.php
@@ -221,19 +221,19 @@
                             </a>
                             <ul class="nav nav-treeview">
                                 <li class="nav-item">
-                                    <a href="./index.html" class="nav-link active">
+                                    <a href="<?php echo base_url(); ?>dashboard1" class="nav-link active">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Dashboard v1</p>
                                     </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="<?php echo base_url(); ?>index.php/dashboard2" class="nav-link">
+                                    <a href="<?php echo base_url(); ?>dashboard2" class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Dashboard v2</p>
                                     </a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="./index3.html" class="nav-link">
+                                    <a href="<?php echo base_url(); ?>dashboard3" class="nav-link">
                                         <i class="far fa-circle nav-icon"></i>
                                         <p>Dashboard v3</p>
                                     </a>
