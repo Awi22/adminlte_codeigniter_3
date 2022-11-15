4af705b5ddf8885d99d1252e402ca60681559986
Awi <102220981+Awi22@users.noreply.github.com>
2022-11-15 16:14:53 +0800

    Update url dashboard3

--- application/views/widgets.php
+++ application/views/widgets.php
@@ -211,7 +211,7 @@
 
         <aside class="main-sidebar sidebar-dark-primary elevation-4">
 
-            <a href="../index3.html" class="brand-link">
+            <a href="<?php echo base_url(); ?>dashboard3" class="brand-link">
                 <img src="<?= base_url('assets'); ?>/vendor/AdminLTE-3.2.0/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
                 <span class="brand-text font-weight-light">AdminLTE 3</span>
             </a>
